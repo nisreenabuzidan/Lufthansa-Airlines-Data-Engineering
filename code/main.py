5ffc1642977fc11d1159c337fe348fc29d9b288d
Nisreen Abu Zidan <nisreen@Ghassans-MBP.fritz.box>
2022-12-02 09:29:55 +0100

    updata Docker-Compose by addinf DB credentials

--- code/main.py
+++ code/main.py
@@ -1,4 +1,5 @@
 import os
+import datetime
 from get_token import get_access_token
 
 
@@ -19,6 +20,7 @@ output = '''
 ============================
     get_access_token test
 ============================
+ current_time = {current_time}
  client_id = {client_id}
  client_secret = {client_secret}
  grant_type = {grant_type}
@@ -30,5 +32,27 @@ output = '''
 
 
 with open('/logs/test_docker.log', 'a') as file:
-    file.write(output.format(client_id=client_id,client_secret=client_secret,grant_type=grant_type,access_token=access_token,token_type=token_type,headers=headers))
-#import get_customer_flight_information_on_departures()
+    file.write(output.format(current_time=datetime.datetime.now(),client_id=client_id,client_secret=client_secret,grant_type=grant_type,access_token=access_token,token_type=token_type,headers=headers))
+
+
+mongo_db_username = os.environ.get("MONGO_INITDB_ROOT_USERNAME")
+mongo_db_password = os.environ.get("MONGO_INITDB_ROOT_PASSWORD")
+port = ""
+
+output = '''
+============================
+    mongodb_connection test
+============================
+ mongo_db_username = {mongo_db_username}
+ mongo_db_password = {mongo_db_password}
+ port = {port}
+
+ 
+'''
+
+
+with open('/logs/test_docker.log', 'a') as file:
+    file.write(output.format(mongo_db_username=mongo_db_username,mongo_db_password=mongo_db_password,port=port))
+
+
+    #import get_customer_flight_information_on_departures()
